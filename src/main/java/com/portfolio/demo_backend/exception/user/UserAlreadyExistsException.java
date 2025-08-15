576d6909814c7123ff1407f7efa8fae14740bce0
TimVandeput <tim.vandeput@student.ucll.be>
2025-08-15 15:28:53 +0200

    cleanup repo merge

--- src/main/java/com/portfolio/demo_backend/exception/user/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.demo_backend.exception.user;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String username) {
-        super("User already exists with username: " + username);
-    }
-}
